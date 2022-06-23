5591eff310a4721a69b62e8220a770f3d30feb8b
Fabiano Shimura <fabiano.shimura@usp.br>
2022-06-23 20:30:55 -0300

    preparando para heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/observatorio-smped'));
+app.use(express.static('./dist/novo-layout'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/observatorio-smped/'}),
+    res.sendFile('index.html', {root: 'dist/novo-layout/'}),
 );
 
 // Start the app by listening on the default Heroku port
