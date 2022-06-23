8658d81118f282f7a0b0ac8b8dfaa00b838ba113
Fabiano Shimura <fabiano.shimura@usp.br>
2022-06-23 20:59:43 -0300

    he

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/novo-layout'));
+app.use(express.static('./dist/portal-front'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/novo-layout/'}),
+    res.sendFile('index.html', {root: 'dist/portal-front/'}),
 );
 
 // Start the app by listening on the default Heroku port
