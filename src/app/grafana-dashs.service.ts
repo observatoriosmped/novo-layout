d7089b828f668682da7a0ed4cd8a384e4b7cbb03
Fabiano Shimura <fabiano.shimura@usp.br>
2022-07-12 13:11:38 -0300

    Nginx headers no servidor
    No Servidor n deu certo pq não reconhece --proxy-config proxy.config.json. Tentando agora usando header no Nginx

--- src/app/grafana-dashs.service.ts
+++ src/app/grafana-dashs.service.ts
@@ -18,15 +18,15 @@ export class GrafanaDashsService {
   //  return this.http.get(url, {headers: headers});    
   //}
 
-  //getData()
-//  {
-//    let url="https://f5aa-143-107-167-161.sa.ngrok.io/api/search"
-//    const headers = new HttpHeaders({
-//      'Content-Type': 'application/json',
-//      'Authorization': `Bearer eyJrIjoieTFPRWZGb21sWDBUZDd0N1RvZWlvUVB2dk1ieFh2b3EiLCJuIjoib2JzZXJ2YXRvcmlvIiwiaWQiOjF9`
-//    }) 
-//    return this.http.get(url, {headers: headers});    
-//  }
+  getData()
+  {
+    let url="https://f5aa-143-107-167-161.sa.ngrok.io/api/search"
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer eyJrIjoieTFPRWZGb21sWDBUZDd0N1RvZWlvUVB2dk1ieFh2b3EiLCJuIjoib2JzZXJ2YXRvcmlvIiwiaWQiOjF9`
+    }) 
+    return this.http.get(url, {headers: headers});    
+  }
 
 //getData()
 //  {
@@ -38,15 +38,15 @@ export class GrafanaDashsService {
 //    return this.http.get(url, {headers: headers});    
 //  }
 
-  getData()
-  {
-    let url="https://observatorio.smped.prefeitura.sp.gov.br/api/search"
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer eyJrIjoieTFPRWZGb21sWDBUZDd0N1RvZWlvUVB2dk1ieFh2b3EiLCJuIjoib2JzZXJ2YXRvcmlvIiwiaWQiOjF9`
-    }) 
-    return this.http.get(url, {headers: headers});    
-  }
+//  getData()
+//  {
+//    let url="https://observatorio.smped.prefeitura.sp.gov.br/api/search"
+//    const headers = new HttpHeaders({
+//      'Content-Type': 'application/json',
+//     'Authorization': `Bearer eyJrIjoieTFPRWZGb21sWDBUZDd0N1RvZWlvUVB2dk1ieFh2b3EiLCJuIjoib2JzZXJ2YXRvcmlvIiwiaWQiOjF9`
+//    }) 
+//    return this.http.get(url, {headers: headers});    
+//  }
 
 
 }
