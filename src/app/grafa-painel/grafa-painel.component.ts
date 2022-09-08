628ba99719be8c084b6510d4cc2846f684e7f2de
Henrique Paes de Souza <hpaesdesouza@gmail.com>
2022-09-08 08:47:18 -0300

    alterando solucao para api do exportador

--- src/app/grafa-painel/grafa-painel.component.ts
+++ src/app/grafa-painel/grafa-painel.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { MatTableDataSource } from '@angular/material/table'
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 import {DashDescriptionService} from '../dash-description.service'
 
@@ -25,11 +26,11 @@ export class GrafaPainelComponent implements OnInit {
   public separator2: number
   public length: number
   safeSrc: SafeResourceUrl;
-  public iframe
-  public proxySource
-  public SafeproxySrc;
+  exportadorURL: string;
+  
 
-  constructor(private responsive: BreakpointObserver, private route: ActivatedRoute, private sanitizer: DomSanitizer, private DetailsGrafana:DashDescriptionService,) {
+  constructor(private responsive: BreakpointObserver, private route: ActivatedRoute, private sanitizer: DomSanitizer, private DetailsGrafana:DashDescriptionService,
+    private http: HttpClient) {
     
   }
 
@@ -44,10 +45,8 @@ export class GrafaPainelComponent implements OnInit {
     this.source = "https://6d5a-143-107-167-161.sa.ngrok.io/d" + this.url + "?orgId=1" + "&kiosk=true"
     //this.source = "http://localhost:3000/d" + this.url + "?orgId=1" + "&kiosk=true"
     this.safeSrc =  this.sanitizer.bypassSecurityTrustResourceUrl(this.source)
-    this.proxySource = "http://localhost/d" + this.url + "?orgId=1";
-    this.SafeproxySrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.proxySource);
+    this.exportadorURL = "";
 
-    console.log(this.SafeproxySrc);
 
     this.DetailsGrafana.getDescription(this.uid).subscribe(data=>{
       this.description.push(data)
@@ -64,56 +63,13 @@ export class GrafaPainelComponent implements OnInit {
       }
     });
   }
+  //Chama a API do exportador
+  fazerDownload(tituloDoGrafico){
 
-  // Botão clicado é a variável que representa o id do botao clicado, retornado via HTML
-  abrirMenu(botaoClicado){
-    let iframe = document.getElementsByTagName("iframe")[0].contentWindow.document;
-    let target;
-    // iframe contem todo o HTML carregado pelo grafana
-    let graficos = iframe.getElementById('reactRoot').getElementsByTagName("h2");
-    // graficos contem um vetor com os elementos h2 que são os titulos de todos os graficos
-    for(let i = 0; i < graficos.length; i++){
-      if(graficos[i].innerHTML.localeCompare(botaoClicado) == 0){
-        target = graficos[i];
-      }
-    }
-    if(!this.menuAberto){ // abre o menu se este não estiver aberto
-      target.click();
-      this.menuAberto = !this.menuAberto;
-    }
-    // Aguardar a operação de abrir o menu, para não buscar os itens vazios
-    setTimeout(() => {
-      let opcoes = iframe.getElementById('reactRoot').getElementsByClassName("dropdown-item-text");
-      let botao;
-      let escolha = new String();
-      for(let i = 0; i < opcoes.length; i++){
-        escolha = <String> opcoes[i].innerHTML;
-        if(escolha.includes("Inspect")){
-          botao = opcoes[i];
-        }
-      }
-      botao.click();
-      this.fazerDownload(botaoClicado, iframe);
-    }
-      ,800);
-  
-  }
-  //Esta funcao é chamada quando o menu inspect do grafana foi aberto
-  fazerDownload(botaoApertado, telaGraficos){
-    let botaoFechar = telaGraficos.getElementsByTagName("button")[1];
-    let botaoDownload = telaGraficos.getElementsByTagName("button")[2];
-    botaoDownload.click();
-    setTimeout(()=>{
-      botaoFechar.click();
-      this.menuAberto = false;
-      this.retornarFoco(botaoApertado);
-    }, 200);
-  }
-  // Retornar foco para o botão que foi clicado para fins de acessibilidade
-  retornarFoco(botaoClicado){
-    let botao = document.getElementById(botaoClicado);
-    console.log(botao);
-    botao.focus();
+    const params = new HttpParams({fromString: 'titulo=' + tituloDoGrafico});
+    const exportadorRequest = this.http.get(this.exportadorURL, {params});
+
+    exportadorRequest.subscribe();
   }
 }
   
